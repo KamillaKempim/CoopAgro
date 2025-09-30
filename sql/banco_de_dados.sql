822ec214042dd88164f239554dd0f6540d30c073
Jhielegend <jhiewertton1@gmail.com>
2025-09-29 23:55:11 -0400

    atualizaco banco de dados

--- sql/banco_de_dados.sql
+++ sql/banco_de_dados.sql
@@ -18,15 +18,18 @@ CREATE TABLE produtos (
 );
 
 -- Cria a tabela de usuários
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     celular VARCHAR(20) NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    rua VARCHAR(150) NOT NULL,
-    cep VARCHAR(10) NOT NULL,
-    numero VARCHAR(10) NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    numero VARCHAR(20) NOT NULL,
     municipio VARCHAR(100) NOT NULL,
+    tipo_usuario ENUM('Produtor', 'Comerciante') NOT NULL,
+    cpf_cnpj VARCHAR(18) NOT NULL UNIQUE,
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
