54c7c59626805b8a773a2a36b8bbe6b1f2d8fd9e
Jhielegend <jhiewertton1@gmail.com>
2025-10-01 23:27:50 -0400

    organizacao dos arquivos de style

--- login.php
+++ login.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - CoopAgro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
 </head>
 <body class="bg-light">
   <header>
